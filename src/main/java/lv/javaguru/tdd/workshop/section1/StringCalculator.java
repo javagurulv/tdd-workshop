ff42984dab343fc2b7fa0a33f03f714382094aeb
Viktors.Savonins <javagurulv@gmail.com>
2015-12-11 10:57:42 +0200

    test case of an empty string implementation refactoring

--- src/main/java/lv/javaguru/tdd/workshop/section1/StringCalculator.java
+++ src/main/java/lv/javaguru/tdd/workshop/section1/StringCalculator.java
@@ -2,13 +2,19 @@ package lv.javaguru.tdd.workshop.section1;
 
 public class StringCalculator {
 
+    private static final String EMPTY_STRING = "";
+
     public int add(String numbers) {
 
-        if ("".equals(numbers)) {
+        if (isEmptyString(numbers)) {
             return 0;
         }
 
         throw new IllegalArgumentException();
+    }
+
+    private boolean isEmptyString(String str) {
+        return EMPTY_STRING.equals(str);
     }
 
 }
