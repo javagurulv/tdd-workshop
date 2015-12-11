611a1364ed624e00471f4cc1e9c478e9d61b20b2
Viktors.Savonins <javagurulv@gmail.com>
2015-12-11 12:17:58 +0200

    @Test whenNumberIsNegativeThenThrowException() implementation

--- src/main/java/lv/javaguru/tdd/workshop/section1/StringCalculator.java
+++ src/main/java/lv/javaguru/tdd/workshop/section1/StringCalculator.java
@@ -2,6 +2,10 @@ package lv.javaguru.tdd.workshop.section1;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
 
 public class StringCalculator {
 
@@ -9,19 +13,40 @@ public class StringCalculator {
     private static final String DELIMITERS_REGEXP = "[,\n]";
 
     public int add(String numbers) {
-        return getSeparatedNumbers(numbers).stream()
+        List<String> splitedNumbers = splitNumbers(numbers);
+
+        List<Integer> negativeNumbers = splitedNumbers.stream()
+                .filter(number -> !isEmptyString(number))
+                .map(Integer::parseInt)
+                .filter(this::isNegativeNumber)
+                .collect(toList());
+
+        if (!negativeNumbers.isEmpty()) {
+            String errorMessage = "negatives not allowed: "
+                    + negativeNumbers.stream()
+                    .map(Object::toString)
+                    .collect(Collectors.joining(", "));
+            throw new IllegalArgumentException(errorMessage);
+        }
+
+        return splitedNumbers.stream()
                 .filter(number -> !isEmptyString(number))
                 .mapToInt(Integer::parseInt)
+                .filter(number -> !isNegativeNumber(number))
                 .sum();
     }
 
-    private List<String> getSeparatedNumbers(String numbers) {
+    private List<String> splitNumbers(String numbers) {
         String[] separatedNumbers = numbers.split(DELIMITERS_REGEXP);
         return Arrays.asList(separatedNumbers);
     }
 
     private boolean isEmptyString(String str) {
         return EMPTY_STRING.equals(str);
+    }
+
+    private boolean isNegativeNumber(int number) {
+        return number < 0;
     }
 
 }
