37be2b0efcf2c647e40fd7976cd806d0110dd7e3
Viktors.Savonins <javagurulv@gmail.com>
2015-12-11 13:29:57 +0200

    section 2 task

---
+++ src/main/java/lv/javaguru/tdd/workshop/section2/EnterpriseStringCalculator.java
@@ -0,0 +1,68 @@
+package lv.javaguru.tdd.workshop.section2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+
+public class EnterpriseStringCalculator {
+
+    private static final String EMPTY_STRING = "";
+    private static final String DELIMITERS_REGEXP = "[,\n]";
+    private static final int ONE_THOUSAND = 1000;
+    private static final int ZERO = 0;
+
+    enum Sign {
+        POSITIVE,
+        NEGATIVE;
+
+        static Sign getSign(int number) {
+            return number < 0 ? NEGATIVE : POSITIVE;
+        }
+
+    }
+
+    public int add(String lineWithNumbers) {
+        Map<Sign, List<Integer>> signedNumbers = splitNumbers(lineWithNumbers).stream()
+                .filter(this::isNumberLessThan1000)
+                .collect(groupingBy(Sign::getSign));
+
+        if (signedNumbers.containsKey(Sign.NEGATIVE)) {
+            List<Integer> negativeNumbers = signedNumbers.get(Sign.NEGATIVE);
+            String errorMessage = "negatives not allowed: "
+                    + negativeNumbers.stream()
+                    .map(Object::toString)
+                    .collect(Collectors.joining(", "));
+            throw new IllegalArgumentException(errorMessage);
+        }
+
+        if (signedNumbers.containsKey(Sign.POSITIVE)) {
+            return signedNumbers.get(Sign.POSITIVE).stream()
+                    .map(Function.identity())
+                    .reduce(ZERO, (a, b) -> a + b);
+        } else {
+            return ZERO;
+        }
+    }
+
+    private List<Integer> splitNumbers(String numbers) {
+        String[] separatedNumbers = numbers.split(DELIMITERS_REGEXP);
+        return Arrays.asList(separatedNumbers).stream()
+                .filter(number -> !isEmptyString(number))
+                .map(Integer::parseInt)
+                .collect(toList());
+    }
+
+    private boolean isEmptyString(String str) {
+        return EMPTY_STRING.equals(str);
+    }
+
+    private boolean isNumberLessThan1000(int number) {
+        return number <= ONE_THOUSAND;
+    }
+
+}
---
+++ src/test/java/lv/javaguru/tdd/workshop/section2/EnterpriseStringCalculatorTest.java
@@ -0,0 +1,86 @@
+package lv.javaguru.tdd.workshop.section2;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class EnterpriseStringCalculatorTest {
+
+    private EnterpriseStringCalculator calculator;
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+
+    @Before
+    public void init() {
+        calculator = new EnterpriseStringCalculator();
+    }
+
+    @Test
+    public void whenNumbersStringIsEmptyThenResultMustBeZero() {
+        int result = calculator.add("");
+        assertThat(result, is(0));
+    }
+
+    @Test
+    public void whenNumbersStringContainsOneNumberThenResultMustBeThatNumber() {
+        int result = calculator.add("1");
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void whenNumbersStringContainsTwoNumbersSeparatedByCommaThenResultMustBeSumOfThisNumbers() {
+        int result = calculator.add("1,2");
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenNumbersStringContainsThreeNumbersSeparatedByCommaThenResultMustBeSumOfThisNumbers() {
+        int result = calculator.add("1,2,3");
+        assertThat(result, is(6));
+    }
+
+    @Test
+    public void whenNumbersSeparatedByNewLineSymbolThenResultMustBeSumOfThisNumbers() {
+        int result = calculator.add("1\n2\n3");
+        assertThat(result, is(6));
+    }
+
+    @Test
+    public void whenNumbersSeparatedByNewLineAndCommaSymbolsThenResultMustBeSumOfThisNumbers() {
+        int result = calculator.add("1\n2,3");
+        assertThat(result, is(6));
+    }
+
+    @Test
+    public void whenNumberIsNegativeThenThrowException() {
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("negatives not allowed: -1");
+        calculator.add("-1");
+    }
+
+    @Test
+    public void whenMultipleNumbersIsNegativeThenThrowExceptionAndShowNegativeNumbersInTheMessage() {
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("negatives not allowed: -1, -2");
+        calculator.add("-1,-2");
+    }
+
+    @Test
+    public void whenNumberIsBiggerThen1000ThenJustIgnoreIt() {
+        int result = calculator.add("1001");
+        assertThat(result, is(0));
+    }
+
+    @Test
+    public void whenAnyNumberIsBiggerThen1000ThenJustIgnoreIt() {
+        int result = calculator.add("1,2,1001,3");
+        assertThat(result, is(6));
+    }
+
+}
